fa53a17a7b851b59085758038f78d2c3434f04b6
Vinicius Nogueira Costa <vinicius.costa@unico.io>
2023-05-12 08:34:43 -0300

    feat: create item function

--- main.go
+++ main.go
@@ -2,28 +2,39 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-var db = initData()
+type Item struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+var items = []Item{
+	{0, "Vinicius"},
+	{1, "Naiara"},
+}
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/list", listItemsHandler).Methods("GET")
+	router.HandleFunc("/create", createItemHandler).Methods("POST")
 	http.ListenAndServe(":8000", router)
-}
-
-func initData() map[int]string {
-	db := map[int]string{
-		0: "Vinicius",
-		1: "Naiara",
-	}
-	return db
 }
 
 func listItemsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(db)
+	json.NewEncoder(w).Encode(items)
+}
+
+func createItemHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	reqBody, _ := io.ReadAll(r.Body)
+	var newItem Item
+	json.Unmarshal(reqBody, &newItem)
+	items = append(items, newItem)
+	json.NewEncoder(w).Encode(items)
 }
